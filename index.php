f814c2a59ea7e35cd04008a986f0a7653e3c3785
hamza-crypto <6793sidique@gmail.com>
2023-02-25 22:39:55 +0500

    First

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Hello World';
+
+?>
+<html>
+    <head>
+        <title>My First PHP Page</title>
+    </head>
+    <body>
+        <h1>My First PHP Page</h1>
+        <p>My first paragraph.</p>
+    </body>
+</html>
+
+
+
